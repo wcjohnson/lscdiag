a65e3fcbc3581bc628b59b1dc08de48a9b2b924c
William C. Johnson <wcjohnson@oigroup.net>
2017-06-13 01:45:14 -0400

    2.0.0-beta.4
    - Support configurable compilers
    - Remove obsolte compilers
    - Prettier!

---
+++ compilers/configurableCompilerShim.js
@@ -0,0 +1,57 @@
+var transform = require('babel-standalone').transform;
+
+function configurableCompilerShim(compilerPlugin, metadata) {
+  var parser = metadata.parser;
+
+  var compile = function compile(src, opts) {
+    var ast, code, map, rst;
+
+    // default opts
+    opts = Object.assign({}, opts);
+    if (!opts.plugins) opts.plugins = [];
+    if (opts.compilerPluginOpts) {
+      opts.plugins.unshift([compilerPlugin, opts.compilerPluginOpts])
+      delete opts.compilerPluginOpts
+    } else {
+      opts.plugins.unshift(compilerPlugin);
+    }
+
+    opts.sourceMaps = true;
+
+    try {
+      rst = transform(src, opts);
+      ast = rst.ast; code = rst.code; map = rst.map;
+    } catch (err) {
+      return {
+        lsc: src,
+        errorMessage: err.message,
+        errorMarker: err.loc
+      };
+    }
+
+    return { js: code, lsc: src, ast: ast, map: map };
+  }
+
+  var parse = function parse(code, opts) {
+    var ast;
+
+    var parserOpts = metadata.getParserOpts(opts);
+    console.log("parserOpts", parserOpts);
+
+    try {
+      ast = parser.parse(code, parserOpts);
+    } catch (err) {
+      return {
+        lsc: code,
+        errorMessage: err.message,
+        errorMarker: err.loc
+      };
+    }
+
+    return { lsc: code, ast: ast };
+  }
+
+  return { compile: compile, parse: parse, metadata: metadata }
+}
+
+module.exports = configurableCompilerShim
--- compilers/index.js
+++ compilers/index.js
@@ -3,7 +3,8 @@ var compilers = {};
 function addCompiler(name, module) {
   compilers[name] = {
     compile: module.compile,
-    parse: module.parse
+    parse: module.parse,
+    metadata: module.metadata
   }
 }
 
@@ -11,25 +12,20 @@ addCompiler(
   "lightscript@0.5.9",
   require("./lightscript_0_5_9/compiler.js")
 );
-addCompiler(
-  "oigroup@1.0.4",
-  require("./oigroup_lightscript_1_0_4/compiler.js")
-);
+
 addCompiler(
   "oigroup@1.1.0",
   require("./oigroup_lightscript_1_1_0/compiler.js")
 );
+
 addCompiler(
-  "oigroup@2.0.0-alpha.1",
-  require("./oigroup_lightscript_2_0_0_alpha_1/compiler.js")
+  "oigroup@2.0.0-beta.1",
+  require("./oigroup_lightscript_2_0_0_beta_1/compiler.js")
 );
+
 addCompiler(
-  "oigroup@2.0.0-alpha.2",
-  require("./oigroup_lightscript_2_0_0_alpha_2/compiler.js")
-);
-addCompiler(
-  "oigroup@2.0.0-alpha.3",
-  require("./oigroup_lightscript_2_0_0_alpha_3/compiler.js")
+  "oigroup@2.0.0-beta.4",
+  require("./oigroup_lightscript_2_0_0_beta_4/compiler.js")
 );
 
 compilers["latest"] = compilers["oigroup@1.1.0"]
