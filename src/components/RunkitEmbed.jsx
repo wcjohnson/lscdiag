ba35ad0dbfa0d7aebdee5574e7166b28c27df202
Coop <coop.coding@gmail.com>
2018-09-14 09:59:12 +1000

    Removed the extra media query that I had added. Removed the run button. Added a RunkitEmbed react component.

--- src/components/RunkitEmbed.jsx
+++ src/components/RunkitEmbed.jsx
@@ -1,9 +1,43 @@
 import React, { Component } from 'react'
-
+import PropTypes from 'prop-types'
 
 class RunkitEmbed extends Component {
-
+	componentDidUpdate() {
+		if (!this.props.source) return
+		this.notebook.setSource(this.props.source)
+		this.notebook.evaluate()
+	}
+	componentDidMount() {
+		this.notebook = RunKit.createNotebook({
+      element: this.refs.notebook,
+			...this.props,
+      onLoad: notebook => notebook.evaluate(),
+    })
+	}
+	render() {
+		return (
+			<div ref='notebook' />
+		)
+	}
 }
 
+RunkitEmbed.propTypes = {
+	source: PropTypes.string,
+	readOnly: PropTypes.bool,
+	mode: PropTypes.string,
+	nodeVersion: PropTypes.string,
+	env: PropTypes.array,
+	title: PropTypes.string,
+	minHeight: PropTypes.string,
+	packageTimestamp: PropTypes.string,
+	preamble: PropTypes.string,
+	onLoad: PropTypes.func,
+	onURLChanged: PropTypes.func,
+	onEvaluate: PropTypes.func
+}
+
+RunkitEmbed.defaultProps = {
+  title: 'LightScript Compiler Output',
+}
 
 export default RunkitEmbed
