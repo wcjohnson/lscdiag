a3a999380f2d3819805afd45a4dd2c0a9157d58a
William Johnson <wcjohnson@oigroup.net>
2018-09-28 21:53:28 -0700

    chore: Delete compilers

--- compilers/oigroup_lightscript_4_0_0/compiler.js
+++
@@ -1,8 +0,0 @@
-var compiler = require('./node_modules/@lightscript/transform');
-var getMetadata = require('./node_modules/@lightscript/transform').api.tooling.getCompilerMetadata;
-
-var makeShim = require('../b7CompilerShim')
-
-var shim = makeShim(compiler, getMetadata());
-
-module.exports = shim;
